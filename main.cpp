759b52dfe8ff37dca1f3f068acef132aa41d51b7
convivae <l16271012@gmail.com>
2020-05-27 13:20:36 +0800

    bug fixed

--- main.cpp
+++ main.cpp
@@ -2,20 +2,20 @@
 #include "week01/SheepDistribution.h"
 
 int main() {
-    /**
-     * week00 CrossRiver
-     */
-    auto test_week00 = CrossRiver(3, 2);
-    test_week00.run();
+//    /**
+//     * week00 CrossRiver
+//     */
+//    auto test_week00 = CrossRiver(3, 2);
+//    test_week00.run();
 
 
     /**
      * week01 SheepDistribution
      */
     std::ofstream out("../output.txt", std::ios_base::out);
-    auto test_week01 = SheepDistribution(1, 4);
+    auto test_week01 = SheepDistribution(2, 4);
     test_week01.calculate(std::cout);
-    //test_week01.calculate(out);
+    test_week01.calculate(out);
 
     return 0;
 }
